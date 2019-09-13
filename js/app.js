5bd4547282cc5f20a2973fff1128cf592a9d542e
shaik80 <sh.mudassir98@gmail.com>
2019-09-13 16:35:52 +0530

    add

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-funtion (){
-    localStorage.setItem("hello","hello")
-}
+$(function(){
+    // data in attributes
+    // getAttributeData()
+    getJSON()
+    // getLocalData()
+ })
+ function getJSON(){
+    $.getJSON('js/ques.json',function(json){
+        questions = json;
+        // console.log(Object)
+        const keys = Object.keys(questions)
+        // console.log(keys.length);
+        let randIndex = Math.floor(Math.random()*(keys.length))
+        console.log(randIndex);
+        localStorage.setItem("lastname", questions.q1);
+    })
+ }
