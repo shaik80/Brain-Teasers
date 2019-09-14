515200ac3d4f35efa20b17a0616752307d6c269d
shaik80 <sh.mudassir98@gmail.com>
2019-09-14 15:22:57 +0530

    add

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ let time =$("#time").html()
 totalSeconds = 0;
 let socre = 0;
 let question = $("#question");
-// var ul =document.getElementById("ul");
 let ques_no = $("#ques_no");
 let op1 =$("#op1");
 let op2 =$("#op2");
@@ -29,6 +28,7 @@ function startTimer() {
     }
   } 
 function start(index){
+    localStorage.getItem("name")
     $.getJSON('js/ques.json',function(json){
         questions = json;
     Object.keys(questions).length;        
@@ -40,28 +40,8 @@ function start(index){
     op3.html(questions["q"+index].options[2]);
     op4.html(questions["q"+index].options[3]);
 })
+
 }
-$("#next").on('click', () => {    
-    var a =parseInt(ques_no.html())+1
-    ques_no.html(a)
-    start(a);
-    // console.log(a)
-    if(a == 11)
-    {
-        window.location.replace("../index.html");
-    }
-})
-$("#prev").on('click', () => {    
-    
-    var a =parseInt(ques_no.html())-1
-    ques_no.html(a)
-    start(a);
-    if(a == 0)
-    {
-        window.location.replace("../index.html");
-    }
-    
-})
 function event_pointer_none(){
     $(".op1").css("pointerEvents" , "none");
     $(".op2").css("pointerEvents" , "none");
@@ -70,13 +50,37 @@ function event_pointer_none(){
 }
 function event_pointer_auto(){
     $(".op1").css("pointerEvents" , "auto");
-    $(".op1").css( "background-color","#ef5350" );
+    $(".op1").css( "background-color","#aee7e8" );
     $(".op2").css("pointerEvents" , "auto");
-    $(".op1").css( "background-color","#ef5350" );
+    $(".op2").css( "background-color","#aee7e8" );
     $(".op3").css("pointerEvents" , "auto");
-    $(".op1").css( "background-color","#ef5350" );
+    $(".op3").css( "background-color","#aee7e8" );
     $(".op4").css("pointerEvents" , "auto");
+    $(".op4").css( "background-color","#aee7e8" );
 }
+$("#next").on('click', () => {    
+    let a =parseInt(ques_no.html())+1
+    ques_no.html(a)
+    start(a);
+    event_pointer_auto()
+    if(a == 11)
+    {
+        window.location.replace("../index.html");
+    }
+})
+$("#prev").on('click', () => {    
+    
+    let a =parseInt(ques_no.html())-1
+    ques_no.html(a)
+    start(a);
+    event_pointer_auto()
+    if(a == 0)
+    {
+        window.location.replace("../index.html");
+    }
+    
+})
+
 function eventpointer(){
     $(".op1").css("pointerEvents" , "none");
     $(".op2").css("pointerEvents" , "none");
@@ -100,71 +104,9 @@ $(".op4").on('click', (v) => {
     $(".op4").css( "background-color","#ef5350" );
     event_pointer_none()    
 })  
-    
-    // let score_board = document.getElementById("socre_no");
-    // let no =ques_no.innerHTML= parseInt(ques_no.innerHTML);   
-    // let b = no-1;
-    // let id = a.id.split('');
-    // let ch = id[2];
 
-    // if( app.questions[b].ans == ch ){
-    //     a.innerHTML = "Correct";
-    //     a.className = "Correct";
-    //     a.style.background="green";
-    //     document.getElementById("socre_no").innerHTML = parseInt(document.getElementById("socre_no").innerHTML)+1;
-    //     // document.getElementById("socre_no") = ;
-    //     console.log(app.questions[index]);
-    // }
-    // else
-    // {
-    //     a.innerHTML = "Worng";
-    //     a.style.background="red";
-    //     console.log("FALSE");
-    //     a.className = "Wrong";
-         
-    // }
-    // for(i=0;i<ul.children.length;i++){
-    //         ul.children[i].style.pointerEvents = "none";
-    //     }
+    $("#submit-name").on('click', () => {
+            console.log(localStorage.setItem("name", $(".info-name").val()))
+            window.location.replace("../question.html");
 
-
-
-
-
-
-
-$(function(){
-    // data in attributes
-    // getAttributeData()
-    getjsonkeys()
-        // getLocalData()
- })
-//  function getJSON(){
-//     $.getJSON('js/ques.json',function(json){
-//         questions = json;
-//         // // console.log(Object)
-//         // let keys = localStorage.setItem("arraykeys" , Object.keys(questions))
-
-//         let getkeys_db =localStorage.getItem("arraykeys").split(",");
-//         // console.log(keys.length);
-        
-//         let randIndex = Math.floor(Math.random()*(getkeys_db.length))
-//         // console.log(questions[res]);
-//         console.log(localStorage.setItem(randIndex, JSON.stringify(questions[getkeys_db[randIndex]])))
-//         let a = getkeys_db[randIndex].map()
-//         // var storedNames = JSON.parse(localStorage.getItem("q1"));
-//         let b = getkeys_db.splice(randIndex, 1)
-//         console.log(localStorage.setItem("arraykeys", getkeys_db))
-//         console.log(a)
-        
-        
-//     })
-//  }
-
- function getjsonkeys(){
-    $.getJSON('js/ques.json',function(json){
-        questions = json;
-        // console.log(Object)
-        Object.keys(questions)     
     })
- }
