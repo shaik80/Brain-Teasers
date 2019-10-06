654bbf3a934ebcabe1a601233906421f5520c72d
shaik80 <sh.mudassir98@gmail.com>
2019-10-06 01:31:35 -0700

    Update questioncontrollers.js

--- controllers/questioncontrollers.js
+++ controllers/questioncontrollers.js
@@ -1,10 +1,10 @@
 const express = require('express');
-let router = express.Router();
+let router = express();
 const mongoose = require('mongoose');
 let Question = mongoose.model('Question');
 
 router.get('/',(req,res) =>{
-    res.redirect("question/index")
+    res.render("question/index")
 });
 
 
